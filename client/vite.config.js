8702b18eda929eaaa7213af6193e811d103f0d35
Priyansh Rathore <priyanshrathore123456@gmail.com>
2025-04-24 18:54:37 +0000

    changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Ensure it's accessible externally
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use the PORT env variable or fallback to 5173
+    allowedHosts: [
+      'micromatch.onrender.com', // Add your Render host here
+    ],
   },
 
   plugins: [
