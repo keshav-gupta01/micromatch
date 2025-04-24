3774b2674bb97dbcbedcec127459f354817e720a
Priyansh Rathore <priyanshrathore123456@gmail.com>
2025-04-24 18:29:30 +0000

    added port in sever in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 5173, // If the PORT env is set, use it; otherwise, default to 5173
+  },
+  
   plugins: [
     react(),tailwindcss()],
 })
