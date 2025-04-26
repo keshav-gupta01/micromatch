93e5aab1c58389dd05f2b842ed5771f421259c57
git-gauravtech <gauravthe7841@gmail.com>
2025-04-26 13:31:03 +0000

    hii

--- backend/controllers/influencerControllers.js
+++ backend/controllers/influencerControllers.js
@@ -20,9 +20,9 @@ exports.verifyInstagram = async (req, res) => {
     const response = await axios.get(`${INSTAGRAM_TOKEN_URL}?code=${code}`);
     const access_token = response.data.access_token
     const insta_scoped_id = response.data.insta_scoped_id
-      
+    
     if (!access_token || !insta_scoped_id) {
-      return res.status(400).json({ success:false ,message: 'Instagram verification failed',access_token,insta_scoped_id});
+      return res.status(400).json({ success:false ,message: 'Instagram verification failed',a : access_token,b : insta_scoped_id});
     }
     return res.json({ success: true, access_token, insta_scoped_id });
   } catch (error) {
