473752e2a8b737c6ae7c60e6114f3b018e46ae86
Priyansh Rathore <priyanshrathore123456@gmail.com>
2025-08-06 14:24:18 +0530

    addedd x-auth-token

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const app = express();
 
 const allowedOrigins = [
   'https://kind-meadow-0a1d96300.1.azurestaticapps.net',
+  'micromatch.onrender.com',
   'http://localhost:5173'
 ];
 
@@ -37,7 +38,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 };
 
 app.use(cors(corsOptions));
